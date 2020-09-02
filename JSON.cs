8733c924a5ec945a068b46c4b3251a143fbc6b02
mariastephan2186 <69307160+mariastephan2186@users.noreply.github.com>
2020-09-02 18:12:37 +0100

    Add files via upload

---
+++ JSON.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTestProject.RestSharpGetEndPoint.Models
+{
+    public class JSONRootObject
+    {
+ 
+
+            public long Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string IpAddress { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            public string City { get; set; }
+        }
+    }
+
+
+
+
